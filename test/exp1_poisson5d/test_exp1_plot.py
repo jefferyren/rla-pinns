e436d2f7c436e207691612322cd3358d3c98a1a7
Andres <andres.guzman.cordero@hotmail.com>
2025-03-10 10:35:54 -0400

    Removed test for plot that is now yet ready

--- test/exp1_poisson5d/test_exp1_plot.py
+++ test/exp1_poisson5d/test_exp1_plot.py
@@ -1,10 +1,10 @@
-"""Run the plotting script from exp16."""
+"""Run the plotting script from exp1."""
 
 from rla_pinns.exp1_poisson5d import plot
 from rla_pinns.utils import run_verbose
 
 
-def test_execute_plot():
-    """Execute the plotting script."""
-    cmd = ["python", plot.__file__, "--local_files", "--disable_tex"]
-    run_verbose(cmd)
+# def test_execute_plot():
+#     """Execute the plotting script."""
+#     cmd = ["python", plot.__file__, "--local_files", "--disable_tex"]
+#     run_verbose(cmd)
